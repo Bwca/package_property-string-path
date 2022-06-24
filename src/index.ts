fec1b535587b7889c018baabb0c92243d7c03611
Volodymyr Yepishev <i.m.bwca@gmail.com>
2022-06-24 20:20:01 +0300

    fixed valueOf and toString appearing when using Date as a type in an interface

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 type Primitive = string | number | bigint | boolean | undefined | symbol;
 
 export type PropertyStringPath<T, Prefix=''> = {
-    [K in keyof T]: T[K] extends Primitive | Array<any> ? `${string & Prefix}${ string & K }` : `${string & Prefix}${ string & K }` | PropertyStringPath <T[K], `${ string & Prefix }${ string & K }.`> ;
+    [K in keyof T]:
+    K extends 'valueOf' | 'toString' ? never :
+    T[K] extends Primitive | Array<any> ? `${string & Prefix}${ string & K }` : `${string & Prefix}${ string & K }` | PropertyStringPath <T[K], `${ string & Prefix }${ string & K }.`> ;
 }[keyof T];
 
 export function propertyStringPathFactory<T, R=string>(): (path: PropertyStringPath<T>) => R {
-	return (path: PropertyStringPath<T>) => (path as unknown as R);
+	// @ts-ignore
+    return (path: PropertyStringPath<T>) => (path as unknown as R);
 }
--- src/test/index.ts
+++ src/test/index.ts
@@ -1,4 +1,4 @@
-import { propertyStringPathFactory } from '../../dist';
+import {PropertyStringPath, propertyStringPathFactory} from '../../dist';
 
 import { User } from './user.model';
 
@@ -6,4 +6,20 @@ const getUserProps = propertyStringPathFactory<User>();
 
 getUserProps('address.geo.lng');
 
-const userPropPath: PropPath<User> = 'address.geo.lng';
+const userPropPath: PropertyStringPath<User> = 'address.geo.lng';
+
+const bookPath = propertyStringPathFactory<Book>()('details.author.name');
+
+
+interface Book {
+    title: string;
+    details: {
+        type: string;
+        published: Date | string;
+        author: {
+            name: string;
+            born: Date | string;
+            died: Date | string | null;
+        };
+    };
+}
